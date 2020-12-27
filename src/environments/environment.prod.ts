af98ff9d34a08536e3721a69ef1d31daefe02694
ramilk2020 <ramil.kirner@gmail.com>
2020-12-27 21:16:18 +0100

    Neu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a4e9ca29dc82045d38b551c2ac4fbd53-1140912641.us-east-2.elb.amazonaws.com:8080/api/v0/'
+  apiHost: 'http://a4e9ca29dc82045d38b551c2ac4fbd53-1140912641.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
